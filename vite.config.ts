71cc404cb206c40097489d0d003d61a19ff81a0d
Thien-Hubbing <triplettran56@gmail.com>
2025-10-05 21:44:46 -0400

    add vite.config.ts
    Signed-off-by: Thien-Hubbing <triplettran56@gmail.com>

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import { fileURLToPath } from "node:url";
+import vue2 from "@vitejs/plugin-vue2";
+
+const config = defineConfig({
+  base: "/AD-multiversal",
+  plugins: [vue2()],
+  resolve: {
+    extensions: [".js", ".ts", ".json", ".vue"],
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
+
+export default config;
