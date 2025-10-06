3d441db7940da135d4095ed15fa6ed1344e9ca26
Thien-Hubbing <triplettran56@gmail.com>
2025-10-05 21:50:42 -0400

    switch over to vite
    Signed-off-by: Thien-Hubbing <triplettran56@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { fileURLToPath } from "node:url";
 import vue2 from "@vitejs/plugin-vue2";
 
 const config = defineConfig({
-  base: "/AD-multiversal",
+  base: "/AD-multiversal/",
   plugins: [vue2()],
   resolve: {
     extensions: [".js", ".ts", ".json", ".vue"],
