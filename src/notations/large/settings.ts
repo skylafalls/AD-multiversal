18cc1a09878045c93a01d94ba881e27d0fd776d6
Skylafalls <triplettran56@gmail.com>
2025-10-26 14:06:18 -0400

    remove the notations
    Signed-off-by: Skylafalls <triplettran56@gmail.com>

--- src/notations/large/settings.ts
+++
@@ -1,7 +0,0 @@
-import Decimal from "break_eternity.js";
-
-export const BigSettings = {
-  isInfinite: (decimal: Decimal): boolean => decimal.gte(Decimal.tetrate(10, 1e16)),
-  numCommas: 100000,
-  exponentDefaultPlaces: 3,
-};
